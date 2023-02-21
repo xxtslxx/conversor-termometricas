283b1e01f5ce6d4793e5cdcf935a09913e886e70
Tassio Lima <83799482+xxtslxx@users.noreply.github.com>
2023-02-21 00:45:24 -0300

    Add files via upload

---
+++ conversor.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const ebulicaoKelvin = 373.0
+
+func main() {
+	kelvin := ebulicaoKelvin
+	celsius := kelvin - 273.0
+	fmt.Printf("A temperatura de ebulição da água em K = %g, temperatura de ebulição da água em °C = %g.", kelvin, celsius)
+
+}
+
+//codigo tassio lima//
